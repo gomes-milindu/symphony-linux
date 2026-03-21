a8c58c83b484197637bceb991a00e5e6e594503c
gomes-milindu <gomesmilindu@gmail.com>
2026-03-21 12:05:48 +0530

    header updated

--- src/assets/pages/Landing.jsx
+++ src/assets/pages/Landing.jsx
@@ -1,12 +1,9 @@
+import Header from "../components/Header";
+
 export default function Landing() {
     return (
         <div className="landing">
-            <h1>Welcome to Symphony</h1>
-            <p>Your one-stop solution for music streaming and discovery.</p>
-            <p>Your one-stop solution for music streaming and discovery.</p>
-
-            <button className="get-started">Get Started</button>
-            <button className="get-started bg-red-400">Get HTTTT</button>
+          <Header />
         </div>
     );
 }
