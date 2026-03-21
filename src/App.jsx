92f5c835bcb145b697c7561267a98e03835ea330
gomes-milindu <gomesmilindu@gmail.com>
2026-03-21 11:19:43 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
-import Homepage from './assets/pages/HomePage'
+import Landing from './assets/pages/Landing'
+
+
 
 function App() {
 
@@ -9,7 +11,7 @@ function App() {
     <>
        <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Homepage />} />
+          <Route path="/" element={<Landing />} />
         
           
         </Routes>
---
+++ src/assets/pages/Landing.jsx
@@ -0,0 +1,9 @@
+export default function Landing() {
+    return (
+        <div className="landing">
+            <h1>Welcome to Symphony</h1>
+            <p>Your one-stop solution for music streaming and discovery.</p>
+            <button className="get-started">Get Started</button>
+        </div>
+    );
+}
