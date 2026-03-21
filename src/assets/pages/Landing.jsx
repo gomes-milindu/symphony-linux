ec9daf72e9fb6fec1825fa9af206c6cc9c7bc602
gomes-milindu <gomesmilindu@gmail.com>
2026-03-21 11:30:23 +0530

    low

--- src/assets/pages/Landing.jsx
+++ src/assets/pages/Landing.jsx
@@ -4,6 +4,7 @@ export default function Landing() {
             <h1>Welcome to Symphony</h1>
             <p>Your one-stop solution for music streaming and discovery.</p>
             <button className="get-started">Get Started</button>
+            <button className="get-started">Get HTTTT</button>
         </div>
     );
 }
