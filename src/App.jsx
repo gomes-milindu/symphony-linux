a339befbef7188186b427ae4051aa674669bc8a6
gomes-milindu <gomesmilindu@gmail.com>
2026-04-10 18:15:30 +0530

    add scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,29 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './App.css'
-import Landing from './assets/pages/Landing'
-import ContactUs from './assets/pages/ContactUs'
-import Gallery from './/assets/pages/Gallery'
-import AboutUs from './assets/pages/AboutUs'
-import Event from './assets/pages/Event'
-import Classes from './assets/pages/Classes'
+// 
 
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Landing from "./assets/pages/Landing";
+import ContactUs from "./assets/pages/ContactUs";
+import Gallery from "./assets/pages/Gallery";
+import AboutUs from "./assets/pages/AboutUs";
+import Event from "./assets/pages/Event";
+import Classes from "./assets/pages/Classes";
+import ScrollToTop from "./assets/components/ScrollToTop";
 
 function App() {
-
-
   return (
-    <>
-       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Landing />} />
+    <BrowserRouter>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Landing />} />
         <Route path="/contactus" element={<ContactUs />} />
         <Route path="/gallery" element={<Gallery />} />
         <Route path="/aboutus" element={<AboutUs />} />
-         <Route path="/events" element={<Event />} />
-         <Route path="/classes" element={<Classes />} />
-          
-        </Routes>
-      </BrowserRouter>
-    </>
-  )
+        <Route path="/events" element={<Event />} />
+        <Route path="/classes" element={<Classes />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
